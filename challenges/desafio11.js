c452e4bf222f3dad3e4ee2a0dd952df7aeb7d292
Victor Emmanuel <victorpassos2012@gmail.com>
2022-06-28 20:10:01 -0300

    Desafios 11 a 15 feitos

---
+++ challenges/desafio11.js
@@ -0,0 +1,12 @@
+db.produtos.find({
+  $and: [
+    { nome: { $ne: "Big Mac" } },
+    { nome: { $ne: "McChicken" } },
+],
+},
+{
+  _id: false,
+  nome: true,
+  curtidas: true,
+  vendidos: true
+});
