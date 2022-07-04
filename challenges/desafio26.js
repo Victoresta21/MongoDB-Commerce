9889b4b683479051a623b890402fb4784ee66751
Victor Emmanuel <victorpassos2012@gmail.com>
2022-07-04 19:56:59 -0300

    Desafio 26 feito

---
+++ challenges/desafio26.js
@@ -0,0 +1,15 @@
+db.produtos.updateMany({
+  $and: [
+    { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gt: 20 } } } },
+    { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $lt: 40 } } } },
+  ],
+}, { $push: { tags: "contém sódio" } });
+
+db.produtos.find(
+  {},
+  {
+    _id: false,
+    nome: true,
+    tags: true,
+},
+);
